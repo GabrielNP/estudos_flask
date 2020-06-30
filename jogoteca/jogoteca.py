8d4d1a47629b2e1848e99ea3ef8945d56d77c28d
GabrielNP <gabrielnp.2010@gmail.com>
2020-06-30 16:03:18 -0300

    Rendering a list with for in html

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html', titulo='Jogos')
+    lista = ['Tetris', 'Super Mario', 'Pokemon Gold']
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
 app.run(host='0.0.0.0', port=8080)
