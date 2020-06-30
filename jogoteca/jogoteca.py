30ceba414bcd16f9609769e489c318b37f965931
GabrielNP <gabrielnp.2010@gmail.com>
2020-06-30 15:53:08 -0300

    Adding dynamic content

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    return render_template('lista.html', titulo='Jogos')
 
 
 app.run(host='0.0.0.0', port=8080)
